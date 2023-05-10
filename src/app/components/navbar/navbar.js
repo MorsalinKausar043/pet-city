85113c4e398c02b6993561b2c3d17ce19b2284ae
Morsalin <morsalinkausar043@gmail.com>
2023-05-10 11:30:27 +0600

    start commit

---
+++ src/app/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-red-900">navbar page</h1>
+      </div>
+    </>
+  );
+}
+
+export default Navbar
---
+++ src/app/signin/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-red-900">footer page</h1>
+      </div>
+    </>
+  );
+}
+
+export default page
